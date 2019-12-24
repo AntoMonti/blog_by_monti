6d40bdfee56c46e6259bbf4c8e40d66f5f00c2dc
AntoMonti <a.matviishyn@hotmail.com>
2019-12-24 16:20:50 +0200

    rectified errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,15 +7,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
-    redirect_to @artile
+    redirect_to @article
   end
 
-  def private
+  private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-  end
 end
