fe6a114df076a722db22c23efd0161892995dc35
AntoMonti <a.matviishyn@hotmail.com>
2020-01-05 13:09:18 +0200

    add some validates for text and subtitle

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 1 }          
+  validates :title, presence: true, length: { minimum: 1 } 
+  validates :text, presence: true, length: { minimun: 1 }
+  validates :subtitle, presence: true, length: {minimun: 1 }         
 end
