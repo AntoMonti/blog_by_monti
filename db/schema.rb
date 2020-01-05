06158f7d2dfa93f21ead85fec8879d8a785b99b6
AntoMonti <a.matviishyn@hotmail.com>
2020-01-05 12:55:17 +0200

    rails db:migrate add column 'subtitle'

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_153208) do
+ActiveRecord::Schema.define(version: 2020_01_05_105056) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "subtitle"
   end
 
   create_table "comments", force: :cascade do |t|
