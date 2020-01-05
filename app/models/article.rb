6db7bba160ce8b92533934a48e2373e00d5c80a3
AntoMonti <a.matviishyn@hotmail.com>
2020-01-05 13:44:43 +0200

    edit syntax errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 1 } 
-  validates :text, presence: true, length: { minimun: 1 }
-  validates :subtitle, presence: true, length: {minimun: 1 }         
+  validates :subtitle, presence: true, length: {minimum: 1 } 
+  validates :text, presence: true, length: { minimum: 1 }
+          
 end
