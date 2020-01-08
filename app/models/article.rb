2c9300c90b765370f1f0692238dbf42a151e9165
AntoMonti <a.matviishyn@hotmail.com>
2020-01-08 09:19:47 +0200

    has_rich_text :text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_rich_text :text
+
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 1 } 
   validates :subtitle, presence: true, length: {minimum: 1 } 
